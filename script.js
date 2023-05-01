9926911902296306663f91ecbd096f4980da0354
Ashmit k <ashmithkacheri138@gmail.com>
2023-05-01 11:23:58 +0530

    Added some features. Close Toggle added Modified mobile design layout

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
 const toggleLogo = document.getElementById('openMenu');
+const closeToggle= document.getElementById('closeMenu')
 const menu = document.querySelector('.navbar');
  
 
 toggleLogo.addEventListener('click',function(){
-    menu.classList.toggle ('active')
+    menu.classList.add ('active')
+})
+
+closeToggle.addEventListener('click',function(){
+    menu.classList.remove('active')
 })
